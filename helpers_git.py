42853a5de60774be183bd8a58fc960ffee3d1e3c
Vladimir Samoylov <cageyv.dev@gmail.com>
2021-11-30 19:59:58 +0700

    fix allowed diff_info len

--- helpers_git.py
+++ helpers_git.py
@@ -44,9 +44,9 @@ def generate_diff(base_branch, current_commit_id, repo_url=''):
     # Truncate too long messages to prevent Slack Api error msg_too_long https://api.slack.com/methods/chat.postMessage#errors
     # Message must be less than 3001 character
     # So number is more or less made up and needs further verification.
-    if len(diff_info) > 2000:
-        diff_info = diff_info[:2000]
-        diff_info += '\ntoo diff info - the rest was truncated. Use link below to see full diff\n```\n\n'
+    if len(diff_info) > 2400:
+        diff_info = diff_info[:2400]
+        diff_info += '\n\n diff info was truncated. Use link below to see full diff\n```\n\n'
     else:
         diff_info += '\n```\n\n'
 
